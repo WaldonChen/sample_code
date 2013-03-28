62f671338ef422d86f59a03f7dc84378fa36c5da
Waldon Chen <cjshappy@163.com>
2013-03-28 21:43:47 +0800

    general method to create proc file

--- ldd/debug/dbg_proc.c
+++ ldd/debug/dbg_proc.c
@@ -1,18 +1,27 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
+#include <linux/proc_fs.h>	// for /proc
 
+static char *PROC_FILE_NAME = "driver/dbg_proc";
 
+int dbg_proc_read_proc(char *page, char **start, off_t off,
+		int count, int *eof, void *data)
+{
+
+}
 
 static int __init dbg_proc_init(void)
 {
-
+	create_proc_entry(PROC_FILE_NAME, 0 /* defult mode */,
+			NULL, dbg_proc_read_proc, NULL /* client data */);
+	
 	return 0;
 }
 
 static void __exit dbg_proc_exit(void)
 {
-
+	remove_proc_entry(PROC_FILE_NAME, NULL /* parent dir */);
 }
 
 module_init(dbg_proc_init);
